4105cac013178bee117c892fa4d1afcfae923372
Obafunso Ridwan Adebayo <0xridwanobafunso@gmail.com>
2023-03-14 20:33:16 +0100

    Fixed BreadCrumb Component & Added QCardContainer Component

---
+++ src/components/QCardContainer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card-container"></div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "QCardContainer",
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.card-container {
+  background-color: #fff;
+  margin-top: 20px;
+  padding-left: 1.25rem;
+  padding-right: 1.25rem;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+  border-width: 1px;
+  border-radius: 1.5rem;
+}
+
+@media (min-width: 1024px) {
+  .card-container {
+    padding: 2.5rem;
+  }
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,6 +2,7 @@
   <q-page>
     <div class="main-section">
       <q-breadcrumb></q-breadcrumb>
+      <q-card-container></q-card-container>
     </div>
   </q-page>
 </template>
@@ -9,14 +10,34 @@
 <script>
 import { defineComponent } from "vue";
 import QBreadcrumb from "../components/QBreadcrumb.vue";
+import QCardContainer from "../components/QCardContainer.vue";
 
 export default defineComponent({
   name: "IndexPage",
   components: {
     QBreadcrumb,
+    QCardContainer,
   },
+
   setup() {},
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.main-section {
+  /* padding: 30px 25px; */
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+  padding-top: 1.25rem;
+  padding-bottom: 1.25rem;
+}
+
+@media (min-width: 1024px) {
+  .main-section {
+    padding-left: 1.25rem;
+    padding-right: 1.25rem;
+    padding-top: 2.5rem;
+    padding-bottom: 2.5rem;
+  }
+}
+</style>
