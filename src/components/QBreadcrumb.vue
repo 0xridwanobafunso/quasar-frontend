9defc04c35990a3d6c84de53fa814d2f7ae6a5b3
Obafunso Ridwan Adebayo <0xridwanobafunso@gmail.com>
2023-03-14 20:26:26 +0100

    Added BreadCrumb Component

---
+++ src/components/QBreadcrumb.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- breadcrumb | css -> breadcrumb.css  -->
+  <nav class="breadcrumb" aria-label="Breadcrumb">
+    <span class="breadcrumb-link">
+      <a>Invoices</a>
+    </span>
+    <span class="seperator">/</span>
+    <span class="breadcrumb-link-active">edit invoice (INV-2022-010)</span>
+  </nav>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "QBreadcrumb",
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.breadcrumb {
+  display: flex;
+}
+
+.breadcrumb-link a {
+  text-decoration: none;
+  color: #4d4284;
+  font-weight: bold;
+}
+
+.breadcrumb-link-active {
+  color: #374151;
+}
+
+.seperator {
+  margin-left: 5px;
+  margin-right: 5px;
+  color: #374151;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,20 @@
 <template>
   <q-page>
-    <div class="main-section"></div>
+    <div class="main-section">
+      <q-breadcrumb></q-breadcrumb>
+    </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import QBreadcrumb from "../components/QBreadcrumb.vue";
 
 export default defineComponent({
   name: "IndexPage",
+  components: {
+    QBreadcrumb,
+  },
   setup() {},
 });
 </script>
